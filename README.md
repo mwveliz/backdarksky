b5cac7a4881ca9e579b9c9eea97eab30336cccf4
Miguel Delgado Veliz <miguel.delgado@engiefactory.com>
2019-12-13 00:51:06 -0300

    functional api with READ.me

---
+++ README.md
@@ -0,0 +1,43 @@
+# backend for calling darksky
+
+> POC: Nodejs - Express REST API with redis 
+
+This POC has two endpoints that are called from the frontend react project
+located at https://github.com/mwveliz/frontdarksky
+
+## Usage
+
+to start the project
+
+```
+    npm start
+```
+
+## API
+The two of them are  GET
+```
+http://localhost:5000/darksky/72.31/-53.06
+http://localhost:5000/darksky/-66.56/10.3
+
+```
+
+
+## Install
+
+With [npm](https://npmjs.org/) installed, run
+
+```
+$ npm i
+```
+
+## Acknowledgments
+
+mwveliz@gmail.com
+
+## See Also
+
+Frontend repo: https://github.com/mwveliz/frontdarksky
+## License
+
+GPL V-3.0
+
---
+++ index.js
@@ -0,0 +1,120 @@
+//set up dependencies
+const express = require("express");
+const redis = require("redis");
+const bodyParser = require("body-parser");
+const credentials = require('./apiCredentials.json'); //credentials for Darksy API
+const unirest = require('unirest');
+const retry = require('retry');
+
+// configuration
+const operation = retry.operation({
+  retries: 3,           // try 1 time and retry 2 times if needed, total = 10
+  minTimeout: 1 * 1000, // the number of milliseconds before starting the first retry
+  maxTimeout: 3 * 1000  // the maximum number of milliseconds between two retries
+});
+
+//count number of calls 
+calls = 0;
+
+
+//setup port constants
+const port_redis = process.env.PORT || 6379;
+const port = process.env.PORT || 5000;
+
+//configure redis client on port 6379
+const redis_client = redis.createClient(port_redis);
+
+//configure express server
+const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Middleware Function to Check Cache
+checkCache = (req, res, next) => {
+  const { lat ,lon } = req.params;
+  const ahora = Math.floor(new Date() / 10000000).toString()+'0000';//cada 10k seg
+  redis_client.get(lat+','+lon+','+ahora,
+                                 (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    if (data != null) {
+      console.log("Found cache")
+      res.send(
+      JSON.stringify({
+           lat: lat,
+           lon: lon,
+           temp: data,
+         })
+      
+      );
+    } else {
+      next();
+    }
+  });
+};
+
+
+
+
+//lamado del api
+callApi = (req, res) =>{
+    const { lat, lon } = req.params;
+    console.log(`call number ${calls}`)
+    let request = unirest.get(`https://${credentials.host}/${credentials.apiKey}/${lat},${lon}`);
+    calls = calls + 1
+    request.end(response => {
+        //si hay error o ocurrio 10% dde probabilidad
+        if (response.error || Math.random()<0.1) res.status(500).end(); 
+        
+        const temperature = response.body.currently.temperature; 
+    //add data to Redis
+    const ahora = Math.floor(new Date() / 10000000).toString()+'0000'; //cada 2.7h
+     //redis_client.geoadd('locations', lat, lon,  +lat+','+lon+','+ ahora);
+     redis_client.set(lat+','+lon+','+ ahora, temperature)
+     const data = JSON.stringify({
+           lat: lat,
+           lon: lon,
+           temp: temperature,
+         })
+     
+    //res.json(data);
+     return data;    
+    });
+    
+}
+
+//tolerante a fallos
+faultTolerantApiCall = (input, callback) => {
+  operation.attempt( (currentAttempt) => {
+    callApi(input, (err, result) => {
+      console.log('Current attempt: ' + currentAttempt);
+      if (operation.retry(err)) {  // retry if needed
+          return;
+      }
+      callback(err ? operation.mainError() : null, result);
+    });
+  });
+}
+
+
+
+//  Endpoint:  GET /darksky/:lat:lon
+//  @desc Return lat lon temp json
+app.get("/darksky/:lat/:lon", checkCache,  (req, res) => {
+
+  try {
+    faultTolerantApiCall(req, (err, result) => {
+        console.log(err, result);
+        return res.status(200).json(result)
+      });      
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+});
+
+app.listen(port, () => console.log(`Server running on Port ${port}`));
