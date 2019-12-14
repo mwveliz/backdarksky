48ee806acec8c1615114b621f48bada57f4806e3
Miguel Delgado Veliz <miguel.delgado@engiefactory.com>
2019-12-14 12:48:44 -0300

    fix ports to CORS

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('*', function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "http://localhost:3600");
+res.header("Access-Control-Allow-Origin", "http://localhost:3006");
 res.header("Access-Control-Allow-Headers", "X-Requested-With");
 res.header('Access-Control-Allow-Headers', 'Content-Type');
 res.header('Access-Control-Allow-Credentials', true);
