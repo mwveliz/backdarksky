8048d12dc8c26e37bd02d3a119998758a01448ce
Miguel Delgado Veliz <miguel.delgado@engiefactory.com>
2019-12-14 12:47:33 -0300

    fix ports

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('*', function(req, res, next) {
-//replace localhost:8080 to the ip address:port of your server
-res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+res.header("Access-Control-Allow-Origin", "http://localhost:3600");
 res.header("Access-Control-Allow-Headers", "X-Requested-With");
 res.header('Access-Control-Allow-Headers', 'Content-Type');
 res.header('Access-Control-Allow-Credentials', true);
